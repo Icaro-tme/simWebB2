66e15d5d8b9b5f0f293cdb44eee5d9b5f44c0558
Icaro-tme <icaroabarros@hotmail.com>
2023-11-22 13:51:58 +0000

    dashboard update

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
-import { Box, Grid, Paper, Typography, CssBaseline } from '@mui/material';
+import { AppBar, Box, Grid, Paper, Toolbar, Typography, CssBaseline, Divider } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { NextPage } from 'next';
 
 const defaultTheme = createTheme();
 
 interface DashboardProps {
-  email?: string; // Definindo 'email' como uma propriedade opcional
+  email?: string;
 }
 
 const Dashboard: NextPage<DashboardProps> = ({ email = '' }) => {
@@ -17,17 +17,22 @@ const Dashboard: NextPage<DashboardProps> = ({ email = '' }) => {
   return (
     <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
-      <Box sx={{ flexGrow: 1, p: 3 }}>
-        <Typography variant="h4" gutterBottom>
+      <AppBar position="static" sx={{ marginBottom: 2, backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)', minHeight: 200, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', display: 'flex', justifyContent: 'center' }}>
+        <Toolbar>
+          <Typography variant="h4" color="inherit" sx={{ fontWeight: 'bold', textAlign: 'center' }}>
+            Painel de Controle
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box sx={{ flexGrow: 1, p: 3, textAlign: 'center' }}>
+        <Typography variant="h5" gutterBottom sx={{ marginBottom: 2 }}>
           Seja bem-vindo, @{formattedEmail}!
         </Typography>
-        <Typography variant="h5" gutterBottom>
-          Painel de Controle
-        </Typography>
-        <Grid container spacing={3}>
+        <Divider sx={{ marginY: 4 }} />
+        <Grid container spacing={3} justifyContent="center">
           {Array.from({ length: 6 }).map((_, index) => (
-            <Grid item xs={12} sm={6} md={4} key={index}>
-              <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+            <Grid item xs={12} md={6} key={index}>
+              <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100%', transition: 'transform 0.3s', '&:hover': { transform: 'scale(1.05)' } }}>
                 <Typography variant="h6">Caixa {index + 1}</Typography>
                 <Typography>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -36,15 +41,25 @@ const Dashboard: NextPage<DashboardProps> = ({ email = '' }) => {
             </Grid>
           ))}
         </Grid>
+        <Box component="footer" sx={{ marginTop: 4, textAlign: 'center' }}>
+          <Typography variant="body2" color="text.secondary">
+            {'Direitos Autorais © '}
+            <a href="https://mui.com/" style={{ color: 'inherit' }}>
+              Seu Site
+            </a>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+          </Typography>
+        </Box>
       </Box>
     </ThemeProvider>
   );
 };
 
-// 'getInitialProps' para recuperar o e-mail da query
 Dashboard.getInitialProps = async (context) => {
   const { query } = context;
-  return { email: query.email || '' };
+  const email = typeof query.email === 'string' ? query.email : '';
+  return { email };
 };
 
 export default Dashboard;
