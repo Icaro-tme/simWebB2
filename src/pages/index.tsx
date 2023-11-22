bdac239111934229257f8f04eed8987f3c891c27
Icaro-tme <icaroabarros@hotmail.com>
2023-11-22 11:45:39 +0000

    package

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 
 import React from 'react';
-import { Button } from "@/components/ui/button"
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+
+
 const HomePage: React.FC = () => {
   return (
     <div>
